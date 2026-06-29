4a5ab6b5a28191d27132f5560ec1d88b1e301599
andreibioazum-cell <andreibioazum@gmail.com>
2026-06-29 12:25:55 +0400

    Update math_utils.h

--- math_utils.h
+++ math_utils.h
@@ -4,40 +4,33 @@
 #include <string.h>
 
 void mat4_identity(float* m) {
-    memset(m, 0, 16 * sizeof(float));
-    m[0] = m[5] = m[10] = m[15] = 1.0f;
+    memset(m, 0, 64); m[0]=m[5]=m[10]=m[15]=1.0f;
 }
 
-void mat4_perspective(float* m, float fov, float aspect, float near, float far) {
-    float f = 1.0f / tanf(fov * 0.5f);
-    memset(m, 0, 16 * sizeof(float));
-    m[0] = f / aspect;
-    m[5] = f;
-    m[10] = (far + near) / (near - far);
+void mat4_perspective(float* m, float fov, float aspect, float n, float f) {
+    float S = 1.0f / tanf(fov * 0.5f);
+    memset(m, 0, 64);
+    m[0] = S / aspect;
+    m[5] = S;
+    m[10] = (f + n) / (n - f);
     m[11] = -1.0f;
-    m[14] = (2.0f * far * near) / (near - far);
+    m[14] = (2.0f * f * n) / (n - f);
 }
 
-void mat4_rotate_y(float* m, float angle) {
+void mat4_rotate(float* m, float angle) {
     mat4_identity(m);
-    m[0] = cosf(angle);
-    m[2] = -sinf(angle);
-    m[8] = sinf(angle);
-    m[10] = cosf(angle);
+    m[0] = cosf(angle); m[2] = sinf(angle);
+    m[8] = -sinf(angle); m[10] = cosf(angle);
+    m[5] = cosf(angle * 0.5f); m[6] = sinf(angle * 0.5f); // Добавим вращение и по X
 }
 
-void mat4_translate(float* m, float x, float y, float z) {
-    mat4_identity(m);
-    m[12] = x; m[13] = y; m[14] = z;
-}
-
-void mat4_mul(float* res, float* a, float* b) {
-    float tmp[16];
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 4; j++) {
-            tmp[i*4+j] = a[i*4+0]*b[0*4+j] + a[i*4+1]*b[1*4+j] + a[i*4+2]*b[2*4+j] + a[i*4+3]*b[3*4+j];
+void mat4_mul(float* out, float* a, float* b) {
+    float res[16];
+    for (int c=0; c<4; c++) {
+        for (int r=0; r<4; r++) {
+            res[c*4+r] = a[0*4+r]*b[c*4+0] + a[1*4+r]*b[c*4+1] + a[2*4+r]*b[c*4+2] + a[3*4+r]*b[c*4+3];
         }
     }
-    memcpy(res, tmp, 16 * sizeof(float));
+    memcpy(out, res, 64);
 }
 #endif
