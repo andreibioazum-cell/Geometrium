fc1aa4e26080a669d1f3507aa2073ac9424c7130
andreibioazum-cell <andreibioazum@gmail.com>
2026-06-29 12:38:33 +0400

    Update world.h

--- world.h
+++ world.h
@@ -1,18 +1,18 @@
 #ifndef WORLD_H
 #define WORLD_H
+#include <math.h>
 
-#define WORLD_SIZE 16
-#define CHUNK_H 8
+#define WORLD_SIZE 12
+#define CHUNK_H 6
 
 unsigned char map[WORLD_SIZE][CHUNK_H][WORLD_SIZE];
 
-void generate_world() {
+static void generate_world() {
     for (int x = 0; x < WORLD_SIZE; x++) {
         for (int z = 0; z < WORLD_SIZE; z++) {
-            int height = (int)(sinf(x * 0.5f) * 2.0f + cosf(z * 0.5f) * 2.0f) + 3;
+            int height = (int)(sinf(x * 0.4f) * 1.5f + cosf(z * 0.4f) * 1.5f) + 2;
             for (int y = 0; y < CHUNK_H; y++) {
-                if (y < height) map[x][y][z] = 1; // Блок есть
-                else map[x][y][z] = 0; // Воздух
+                map[x][y][z] = (y < height) ? 1 : 0;
             }
         }
     }
