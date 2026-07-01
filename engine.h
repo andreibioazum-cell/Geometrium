dabaf5c0cca1d6914d7d378f812e2ec4b07b1cff
andreibioazum-cell <andreibioazum@gmail.com>
2026-07-01 20:25:53 +0400

    Update engine.h

--- engine.h
+++ engine.h
@@ -6,33 +6,18 @@
 #include <GLES2/gl2.h>
 #include <stdbool.h>
 #include <stdint.h>
+#include <stdlib.h>
 
-// UI Константы
-#define JOY_RADIUS      80.0f
-#define JOY_X_OFFSET    130.0f
-#define JOY_Y_OFFSET    130.0f
-#define STICK_RADIUS    32.0f
-#define JUMP_BTN_SIZE   80.0f
-#define JUMP_BTN_OFFSET 130.0f
-#define ACTION_BTN_SIZE 45.0f
-#define BREAK_BTN_X     80.0f
-#define BREAK_BTN_Y     80.0f
-#define PLACE_BTN_X     80.0f
-#define PLACE_BTN_Y     190.0f
+#define PI 3.14159265f
+#define WORLD_BUF 65
+#define CHUNK_H 32
+#define LOAD_RADIUS 32
+#define MAX_EDITS 1024
+#define RAY_DIST 6.0f
+#define RAY_STEP 0.05f
 
-#define PI              3.14159265f
-#define PLAYER_W        0.4f
-#define EYE_H           1.65f
-#define HEAD_MARGIN     0.15f
-#define GAME_FOV        1.4915f
-
-#define GRAVITY         0.005f
-#define JUMP_FORCE      0.12f
-#define TERM_VEL       -0.25f
-
-#define LOAD_RADIUS     32
-#define WORLD_BUF       (LOAD_RADIUS * 2 + 1)
-#define CHUNK_H         32
+#define STATE_MENU 0
+#define STATE_PLAYING 1
 
 #define FACE_XP 0x01
 #define FACE_XN 0x02
@@ -41,84 +26,23 @@
 #define FACE_ZP 0x10
 #define FACE_ZN 0x20
 
-#define RAY_DIST        6.0f
-#define RAY_STEP        0.05f
-#define MAX_EDITS       1024
-
-#define BLOCK_AIR       0
-#define BLOCK_GRASS     1
-#define BLOCK_DIRT      2
-#define BLOCK_STONE     3
-
-#define INV_SLOTS       9
-#define INV_SLOT_SIZE   46.0f
-#define INV_PADDING     4.0f
-#define INV_Y_OFFSET    50.0f
-
-#define ANIM_BREAK_FRAMES 12
-#define ANIM_PLACE_FRAMES 10
-
-#define STATE_MENU      0
-#define STATE_PLAYING   1
-
-struct block_edit {
-    int wx, wy, wz;
-    unsigned char val;
-};
+struct block_edit { int wx, wy, wz; unsigned char val; };
 
 struct engine {
     struct android_app* app;
-    EGLDisplay display;
-    EGLSurface surface;
-    EGLContext context;
+    EGLDisplay display; EGLSurface surface; EGLContext context;
     int32_t width, height;
-    
     GLuint program;
-    GLuint uiProgram;
-    GLuint invProgram;
-    GLuint texGrassTop, texGrassSide, texGrassDown;
-    GLuint vbo;
-
-    float camPos[3];
-    float camRot[2];
-    float velY;
-
-    float joyX, joyY;
-    float moveDirX, moveDirZ;
-    float lastTouchX, lastTouchY;
-
-    bool isMoving;
-    int movePointerId;
-    int lookPointerId;
-    bool onGround;
-
-    int visibleFaceCount;
-    bool meshDirty;
-
-    int loadCenterX, loadCenterZ;
-    bool worldLoaded;
-
-    // Динамическая память для мира
-    unsigned char (*blocks)[CHUNK_H][WORLD_BUF];
-    unsigned char (*faces)[CHUNK_H][WORLD_BUF];
-
-    struct block_edit edits[MAX_EDITS];
-    int editCount;
-
-    unsigned char invSlots[INV_SLOTS];
-    int selectedSlot;
-
-    // Анимация
-    bool animActive;
-    bool animIsBreak;
-    int animTimer;
-    float animBlockPos[3];
-    unsigned char animBlockType;
-
-    int gameState;
+    float camPos[3], camRot[2], velY;
+    bool onGround, isMoving, worldLoaded, meshDirty;
+    unsigned char (*blocks)[32][65];
+    unsigned char (*faces)[32][65];
+    int loadCenterX, loadCenterZ, editCount, visibleFaceCount, gameState;
     unsigned int worldSeed;
-    int seedDigits[6];
-    int seedCursor;
+    int seedDigits[6], seedCursor;
+    struct block_edit edits[MAX_EDITS];
+    // Поля для анимации
+    bool animActive; int animTimer; float animBlockPos[3];
 };
 
 #endif
