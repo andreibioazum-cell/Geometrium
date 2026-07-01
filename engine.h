64f99837bff29e1c018b0a80c0c58ff999d2e956
andreibioazum-cell <andreibioazum@gmail.com>
2026-07-01 18:23:26 +0400

    Update engine.h

--- engine.h
+++ engine.h
@@ -5,36 +5,33 @@
 #include <EGL/egl.h>
 #include <GLES2/gl2.h>
 #include <stdbool.h>
+#include <math.h>
 
-/* Джойстик зеркально от прыжка */
-#define JOY_RADIUS      80.0f
-#define JOY_X_OFFSET    130.0f
-#define JOY_Y_OFFSET    130.0f
-#define STICK_RADIUS    32.0f
-
-#define JUMP_BTN_SIZE   80.0f
+#define PI 3.14159265f
+#define JOY_RADIUS 80.0f
+#define JOY_X_OFFSET 130.0f
+#define JOY_Y_OFFSET 130.0f
+#define STICK_RADIUS 32.0f
+#define JUMP_BTN_SIZE 80.0f
 #define JUMP_BTN_OFFSET 130.0f
-
 #define ACTION_BTN_SIZE 45.0f
-#define BREAK_BTN_X     80.0f
-#define BREAK_BTN_Y     80.0f
-#define PLACE_BTN_X     80.0f
-#define PLACE_BTN_Y     190.0f
+#define BREAK_BTN_X 80.0f
+#define BREAK_BTN_Y 80.0f
+#define PLACE_BTN_X 80.0f
+#define PLACE_BTN_Y 190.0f
 
-#define PI              3.14159265f
+#define PLAYER_W 0.4f
+#define EYE_H 1.65f
+#define HEAD_MARGIN 0.15f
+#define GAME_FOV 1.4915f
 
-#define PLAYER_W        0.4f
-#define EYE_H           1.65f
-#define HEAD_MARGIN     0.15f
-#define GAME_FOV        1.4915f
+#define GRAVITY 0.005f
+#define JUMP_FORCE 0.12f
+#define TERM_VEL -0.25f
 
-#define GRAVITY         0.005f
-#define JUMP_FORCE      0.12f
-#define TERM_VEL       -0.25f
-
-#define LOAD_RADIUS     32
-#define WORLD_BUF       (LOAD_RADIUS * 2 + 1)
-#define CHUNK_H         32
+#define LOAD_RADIUS 32
+#define WORLD_BUF (LOAD_RADIUS * 2 + 1)
+#define CHUNK_H 32
 
 #define FACE_XP 0x01
 #define FACE_XN 0x02
@@ -43,83 +40,55 @@
 #define FACE_ZP 0x10
 #define FACE_ZN 0x20
 
-#define RAY_DIST        6.0f
-#define RAY_STEP        0.02f
-#define MAX_EDITS       512
+#define RAY_DIST 6.0f
+#define RAY_STEP 0.02f
+#define MAX_EDITS 512
 
-#define BLOCK_AIR       0
-#define BLOCK_GRASS     1
+#define BLOCK_AIR 0
+#define BLOCK_GRASS 1
 
-#define INV_SLOTS       9
-#define INV_SLOT_SIZE   46.0f
-#define INV_PADDING     4.0f
-#define INV_Y_OFFSET    50.0f
+#define INV_SLOTS 9
+#define INV_SLOT_SIZE 46.0f
+#define INV_PADDING 4.0f
+#define INV_Y_OFFSET 50.0f
 
 #define ANIM_BREAK_FRAMES 12
 #define ANIM_PLACE_FRAMES 8
 
-/* Состояния игры */
-#define STATE_MENU      0
-#define STATE_PLAYING   1
+#define STATE_MENU 0
+#define STATE_PLAYING 1
 
-struct block_edit {
-    int wx, wy, wz;
-    unsigned char val;
-};
+struct block_edit { int wx, wy, wz; unsigned char val; };
 
 struct engine {
     struct android_app* app;
-    EGLDisplay display;
-    EGLSurface surface;
-    EGLContext context;
+    EGLDisplay display; EGLSurface surface; EGLContext context;
     int32_t width, height;
-    GLuint program;
-    GLuint invProgram;
-    GLuint texGrassTop;
-    GLuint texGrassSide;
-    GLuint texGrassDown;
+    GLuint program, invProgram, uiProg;
+    GLuint texGrassTop, texGrassSide, texGrassDown;
 
-    float camPos[3];
-    float camRot[2];
-    float velY;
-
-    float joyX, joyY;
-    float moveDirX, moveDirZ;
-    float lastTouchX, lastTouchY;
-
-    bool isMoving;
-    int movePointerId;
-    int lookPointerId;
-    bool onGround;
+    float camPos[3], camRot[2], velY;
+    float joyX, joyY, moveDirX, moveDirZ, lastTouchX, lastTouchY;
+    bool isMoving, onGround, worldLoaded, meshDirty;
+    int movePointerId, lookPointerId;
 
     GLuint vbo;
     int visibleFaceCount;
-    bool meshDirty;
-
     int loadCenterX, loadCenterZ;
-    bool worldLoaded;
 
     unsigned char blocks[WORLD_BUF][CHUNK_H][WORLD_BUF];
     unsigned char faces[WORLD_BUF][CHUNK_H][WORLD_BUF];
-
     struct block_edit edits[MAX_EDITS];
     int editCount;
 
     unsigned char invSlots[INV_SLOTS];
     int selectedSlot;
 
-    /* Анимация блока */
-    int animBreakTimer;
-    int animPlaceTimer;
+    int animBreakTimer, animPlaceTimer;
     float animBlockX, animBlockY, animBlockZ;
-    bool animActive;
-    bool animIsBreak;
+    bool animActive, animIsBreak;
 
-    /* Меню */
-    int gameState;
-    int worldSeed;
-    int seedDigits[6];
-    int seedCursor;
+    int gameState, worldSeed, seedDigits[6], seedCursor;
 };
 
 #endif
