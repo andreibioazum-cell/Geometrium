6428d5703a0e7c1742af5cb73f5c16d35fce7036
andreibioazum-cell <andreibioazum@gmail.com>
2026-07-10 17:36:41 +0400

    Update render.h

--- render.h
+++ render.h
@@ -65,8 +65,12 @@ void init_textures(struct engine* eng) {
     eng->texLeaves = load_texture(eng->app, "foliage.png");
     eng->texTreeSide = load_texture(eng->app, "tree_side.png");
     eng->texTreeTop = load_texture(eng->app, "tree_top_down.png");
+    if (!eng->texGrassTop) eng->texGrassTop = make_color_tex(100, 180, 60);
+    if (!eng->texGrassSide) eng->texGrassSide = make_color_tex(120, 100, 70);
+    if (!eng->texGrassDown) eng->texGrassDown = make_color_tex(140, 110, 70);
     if (!eng->texLeaves) eng->texLeaves = make_color_tex(40, 120, 40);
     if (!eng->texTreeSide) eng->texTreeSide = make_color_tex(90, 70, 40);
+    if (!eng->texTreeTop) eng->texTreeTop = make_color_tex(100, 80, 50);
 }
 
 static void push_quad_n(float* buf, int* idx, float x0,float y0,float z0,float u0,float v0, float x1,float y1,float z1,float u1,float v1, float x2,float y2,float z2,float u2,float v2, float x3,float y3,float z3,float u3,float v3, float nx,float ny,float nz, float type) {
@@ -86,8 +90,8 @@ static void rebuild_vbo(struct engine* eng) {
             }
     eng->visibleFaceCount = fc;
     if (fc == 0) { eng->meshDirty = false; return; }
-    int cnt = fc * 54;
-    float* buf = (float*)malloc((size_t)cnt * sizeof(float));
+    float* buf = (float*)malloc((size_t)fc * 54 * sizeof(float));
+    if(!buf) return;
     int idx = 0, ox = eng->loadCenterX - LOAD_RADIUS, oz = eng->loadCenterZ - LOAD_RADIUS;
     for (int x = 0; x < WORLD_BUF; x++)
         for (int y = 0; y < CHUNK_H; y++)
@@ -104,31 +108,28 @@ static void rebuild_vbo(struct engine* eng) {
                 if(f&FACE_ZN)push_quad_n(buf,&idx,x0,y0,z1,0,1,x1,y0,z1,1,1,x1,y1,z1,1,0,x0,y1,z1,0,0,0,0,1,type);
             }
     if (!eng->vbo) glGenBuffers(1, &eng->vbo);
-    glBindBuffer(GL_ARRAY_BUFFER, eng->vbo); glBufferData(GL_ARRAY_BUFFER, (GLsizeiptr)(cnt*sizeof(float)), buf, GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, eng->vbo); 
+    glBufferData(GL_ARRAY_BUFFER, (GLsizeiptr)(fc * 54 * sizeof(float)), buf, GL_STATIC_DRAW);
     free(buf); eng->meshDirty = false;
 }
 
-// ПЛАВНАЯ АНИМАЦИЯ МАСШТАБА
 static void render_anim_block(struct engine* eng) {
     if (!eng->animActive) return;
     float scale = 1.0f;
     if (eng->animIsBreak) {
         float t = (float)eng->animBreakTimer / ANIM_BREAK_FRAMES;
-        scale = t * t; // Уменьшение
-        eng->animBreakTimer--;
+        scale = t * t; eng->animBreakTimer--;
     } else {
         float t = (float)eng->animPlaceTimer / ANIM_PLACE_FRAMES;
-        scale = 1.0f - (t * t); // Увеличение
-        eng->animPlaceTimer--;
+        float p = 1.0f - t; scale = p * p; eng->animPlaceTimer--;
     }
     if (eng->animBreakTimer <= 0 && eng->animPlaceTimer <= 0) eng->animActive = false;
-    
     float bx = eng->animBlockX, by = eng->animBlockY, bz = eng->animBlockZ, hs = scale * 0.5f;
-    float abuf[324]; int aidx = 0; float type = 1.0f; // Для анимации используем траву
+    float abuf[324]; int aidx = 0; float type = 1.0f;
     push_quad_n(abuf,&aidx,bx+hs,by-hs,bz-hs,0,1,bx+hs,by-hs,bz+hs,1,1,bx+hs,by+hs,bz+hs,1,0,bx+hs,by+hs,bz-hs,0,0,1,0,0,type);
     push_quad_n(abuf,&aidx,bx-hs,by-hs,bz+hs,0,1,bx-hs,by-hs,bz-hs,1,1,bx-hs,by+hs,bz-hs,1,0,bx-hs,by+hs,bz+hs,0,0,-1,0,0,type);
     push_quad_n(abuf,&aidx,bx-hs,by+hs,bz+hs,0,0,bx+hs,by+hs,bz+hs,1,0,bx+hs,by+hs,bz-hs,1,1,bx-hs,by+hs,bz-hs,0,1,0,1,0,type);
-    push_quad_n(abuf,&aidx,bx-hs,by-hs,bz-hs,0,0,bx+hs,by-hs,bz-hs,1,0,bx+hs,by-hs,bz+hs,1,1,bx-hs,by-hs,bz+hs,0,1,0,-1,0,type);
+    push_quad_n(abuf,&aidx,bx-hs,by-hs,bz-hs,0,0,bx+hs,by-hs,bz-hs,1,0,bx+hs,by-hs,bz+hs,1,1,bx-hs,by+hs,bz+hs,0,1,0,-1,0,type);
     push_quad_n(abuf,&aidx,bx+hs,by-hs,bz-hs,0,1,bx-hs,by-hs,bz-hs,1,1,bx-hs,by+hs,bz-hs,1,0,bx+hs,by+hs,bz-hs,0,0,0,0,-1,type);
     push_quad_n(abuf,&aidx,bx-hs,by-hs,bz+hs,0,1,bx+hs,by-hs,bz+hs,1,1,bx+hs,by+hs,bz+hs,1,0,bx-hs,by+hs,bz+hs,0,0,0,0,1,type);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -144,20 +145,17 @@ void render_world(struct engine* eng) {
     if (eng->meshDirty) rebuild_vbo(eng);
     glEnable(GL_DEPTH_TEST); glUseProgram(eng->program);
     glUniform3f(glGetUniformLocation(eng->program, "camPos"), eng->camPos[0], eng->camPos[1], eng->camPos[2]);
-    
     glActiveTexture(GL_TEXTURE0); glBindTexture(GL_TEXTURE_2D, eng->texGrassTop); glUniform1i(glGetUniformLocation(eng->program, "texTop"), 0);
     glActiveTexture(GL_TEXTURE1); glBindTexture(GL_TEXTURE_2D, eng->texGrassSide); glUniform1i(glGetUniformLocation(eng->program, "texSide"), 1);
     glActiveTexture(GL_TEXTURE2); glBindTexture(GL_TEXTURE_2D, eng->texGrassDown); glUniform1i(glGetUniformLocation(eng->program, "texDown"), 2);
     glActiveTexture(GL_TEXTURE3); glBindTexture(GL_TEXTURE_2D, eng->texLeaves); glUniform1i(glGetUniformLocation(eng->program, "texLeaves"), 3);
     glActiveTexture(GL_TEXTURE4); glBindTexture(GL_TEXTURE_2D, eng->texTreeSide); glUniform1i(glGetUniformLocation(eng->program, "texTreeSide"), 4);
     glActiveTexture(GL_TEXTURE5); glBindTexture(GL_TEXTURE_2D, eng->texTreeTop); glUniform1i(glGetUniformLocation(eng->program, "texTreeTop"), 5);
-    
     float proj[16], view[16], mvp[16];
     mat4_perspective(proj, GAME_FOV, (float)eng->width/(float)eng->height, 0.1f, 200.0f);
     mat4_lookat(view, eng->camPos, eng->camRot[0], eng->camRot[1]);
     mat4_mul(mvp, proj, view);
     glUniformMatrix4fv(glGetUniformLocation(eng->program, "m"), 1, GL_FALSE, mvp);
-    
     if (eng->visibleFaceCount > 0) {
         glBindBuffer(GL_ARRAY_BUFFER, eng->vbo);
         glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,36,(void*)0); glEnableVertexAttribArray(0);
@@ -167,7 +165,8 @@ void render_world(struct engine* eng) {
         glDrawArrays(GL_TRIANGLES, 0, eng->visibleFaceCount * 6);
     }
     render_anim_block(eng);
-    glBindBuffer(GL_ARRAY_BUFFER, 0); glDisableVertexAttribArray(1); glDisableVertexAttribArray(2); glDisableVertexAttribArray(3);
+    glBindBuffer(GL_ARRAY_BUFFER, 0); 
+    glDisableVertexAttribArray(1); glDisableVertexAttribArray(2); glDisableVertexAttribArray(3);
 }
 
 void init_ui_shader(void) {
@@ -240,6 +239,6 @@ void draw_ui(struct engine* eng) {
         else if(eng->invSlots[i] == BLOCK_LEAVES) icon = eng->texLeaves;
         if(icon) draw_rect_tex(sx, sy, INV_SLOT_SIZE*0.35f, INV_SLOT_SIZE*0.35f, sw, sh, icon);
     }
-    glDisable(GL_BLEND);
+    glDisable(GL_BLEND); 
 }
 #endif
