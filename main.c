f2b446a4194888ed0c6ff4349b1b9ac3b5c3402d
andreibioazum-cell <andreibioazum@gmail.com>
2026-07-10 17:41:18 +0400

    Update main.c

--- main.c
+++ main.c
@@ -4,7 +4,6 @@
 #include <android/log.h>
 #include <stdlib.h>
 #include <time.h>
-
 #include "engine.h"
 #include "world.h"
 #include "math_utils.h"
@@ -12,56 +11,35 @@
 #include "input.h"
 #include "render.h"
 
-#define LOG_TAG "Geometrium"
-#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
-
 unsigned int game_seed = 0;
 
 static void engine_draw_frame(struct engine* eng) {
-    if (!eng->display || !eng->surface || eng->program == 0) return;
-    
+    if (!eng->display || !eng->surface) return;
     eglQuerySurface(eng->display, eng->surface, EGL_WIDTH, &eng->width);
     eglQuerySurface(eng->display, eng->surface, EGL_HEIGHT, &eng->height);
     glViewport(0, 0, eng->width, eng->height);
-
     if (eng->gameState == STATE_MENU) {
-        glClearColor(0.2f, 0.6f, 0.3f, 1);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        glEnable(GL_BLEND);
-        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-        draw_menu(eng);
-        eglSwapBuffers(eng->display, eng->surface);
-        return;
+        glClearColor(0.2f, 0.6f, 0.3f, 1); glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glEnable(GL_BLEND); glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+        draw_menu(eng); eglSwapBuffers(eng->display, eng->surface); return;
     }
-
-    update_world(eng);
-    apply_physics(eng);
-
-    glClearColor(0.53f, 0.81f, 0.98f, 1);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    
-    render_world(eng);
-    draw_ui(eng);
-
+    update_world(eng); apply_physics(eng);
+    glClearColor(0.53f, 0.81f, 0.98f, 1); glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    render_world(eng); draw_ui(eng);
     eglSwapBuffers(eng->display, eng->surface);
 }
 
 static void engine_handle_cmd(struct android_app* app, int32_t cmd) {
     struct engine* eng = (struct engine*)app->userData;
     if (cmd == APP_CMD_INIT_WINDOW) {
-        eng->display = eglGetDisplay(EGL_DEFAULT_DISPLAY);
-        eglInitialize(eng->display, NULL, NULL);
-
-        EGLConfig config; EGLint n;
-        EGLint att[] = { EGL_RENDERABLE_TYPE, EGL_OPENGL_ES2_BIT, EGL_DEPTH_SIZE, 16, EGL_NONE };
+        eng->display = eglGetDisplay(EGL_DEFAULT_DISPLAY); eglInitialize(eng->display, NULL, NULL);
+        EGLConfig config; EGLint n; EGLint att[] = { EGL_RENDERABLE_TYPE, EGL_OPENGL_ES2_BIT, EGL_DEPTH_SIZE, 16, EGL_NONE };
         eglChooseConfig(eng->display, att, &config, 1, &n);
         eng->surface = eglCreateWindowSurface(eng->display, config, app->window, NULL);
-        
         EGLint ctxAtt[] = { EGL_CONTEXT_CLIENT_VERSION, 2, EGL_NONE };
         eng->context = eglCreateContext(eng->display, config, NULL, ctxAtt);
         eglMakeCurrent(eng->display, eng->surface, eng->surface, eng->context);
 
-        // УПРОЩЕННЫЙ ШЕЙДЕР (БЕЗ INT)
         const char* vS = 
             "attribute vec3 pos; attribute vec2 uv; attribute vec3 norm; attribute float type;"
             "varying vec2 vUV; varying vec3 vNorm; varying float vType;"
@@ -87,67 +65,31 @@ static void engine_handle_cmd(struct android_app* app, int32_t cmd) {
             "  gl_FragColor = vec4(tc.rgb * light, tc.a);"
             "}";
 
-        GLuint vs = glCreateShader(GL_VERTEX_SHADER);
-        glShaderSource(vs, 1, &vS, NULL); glCompileShader(vs);
-        
-        GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
-        glShaderSource(fs, 1, &fS, NULL); glCompileShader(fs);
-
+        GLuint vs = glCreateShader(GL_VERTEX_SHADER); glShaderSource(vs, 1, &vS, NULL); glCompileShader(vs);
+        GLuint fs = glCreateShader(GL_FRAGMENT_SHADER); glShaderSource(fs, 1, &fS, NULL); glCompileShader(fs);
         eng->program = glCreateProgram();
-        glAttachShader(eng->program, vs);
-        glAttachShader(eng->program, fs);
-        
-        // Принудительная привязка до линковки
-        glBindAttribLocation(eng->program, 0, "pos");
-        glBindAttribLocation(eng->program, 1, "uv");
-        glBindAttribLocation(eng->program, 2, "norm");
-        glBindAttribLocation(eng->program, 3, "type");
-        
+        glAttachShader(eng->program, vs); glAttachShader(eng->program, fs);
+        glBindAttribLocation(eng->program, 0, "pos"); glBindAttribLocation(eng->program, 1, "uv");
+        glBindAttribLocation(eng->program, 2, "norm"); glBindAttribLocation(eng->program, 3, "type");
         glLinkProgram(eng->program);
-
-        init_textures(eng);
-        init_ui_shader();
-        
-        srand(time(NULL));
-        game_seed = (unsigned int)rand();
-        eng->worldSeed = (int)game_seed;
+        init_textures(eng); init_ui_shader();
+        srand(time(NULL)); game_seed = (unsigned int)rand();
     }
 }
 
 void android_main(struct android_app* state) {
     struct engine* eng = (struct engine*)malloc(sizeof(struct engine));
-    if(!eng) return;
     memset(eng, 0, sizeof(struct engine));
-
-    eng->app = state;
-    eng->movePointerId = -1;
-    eng->lookPointerId = -1;
+    eng->app = state; eng->movePointerId = -1; eng->lookPointerId = -1;
     eng->gameState = STATE_MENU;
-
     for(int i=0; i<INV_SLOTS; i++) eng->invSlots[i] = BLOCK_AIR;
-    eng->invSlots[0] = BLOCK_GRASS;
-    eng->invSlots[1] = BLOCK_WOOD;
-    eng->invSlots[2] = BLOCK_LEAVES;
-
-    eng->camPos[1] = 20.0f;
-    state->userData = eng;
-    state->onAppCmd = engine_handle_cmd;
-    state->onInputEvent = engine_handle_input;
-
+    eng->invSlots[0] = BLOCK_GRASS; eng->invSlots[1] = BLOCK_WOOD; eng->invSlots[2] = BLOCK_LEAVES;
+    eng->camPos[1] = 20.0f; state->userData = eng; state->onAppCmd = engine_handle_cmd; state->onInputEvent = engine_handle_input;
     while (1) {
         int ev; struct android_poll_source* s;
         while (ALooper_pollOnce(0, NULL, &ev, (void**)&s) >= 0) {
             if (s) s->process(state, s);
-            if (state->destroyRequested) { 
-                if(eng->display) {
-                    eglMakeCurrent(eng->display, EGL_NO_SURFACE, EGL_NO_SURFACE, EGL_NO_CONTEXT);
-                    eglDestroyContext(eng->display, eng->context);
-                    eglDestroySurface(eng->display, eng->surface);
-                    eglTerminate(eng->display);
-                }
-                free(eng); 
-                return; 
-            }
+            if (state->destroyRequested) { free(eng); return; }
         }
         engine_draw_frame(eng);
     }
