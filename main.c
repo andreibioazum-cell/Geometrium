6a4eaaab041c7a2bbc33edbaceacce65c0dc74f0
andreibioazum-cell <andreibioazum@gmail.com>
2026-07-11 19:49:02 +0400

    Update main.c

--- main.c
+++ main.c
@@ -13,20 +13,6 @@
 #include "input.h"
 #include "render.h"
 
-void copy_to_clipboard(struct android_app* app, const char* text) {
-    JNIEnv* env = NULL;
-    (*app->activity->vm)->AttachCurrentThread(app->activity->vm, &env, NULL);
-    jclass ctx = (*env)->FindClass(env, "android/content/Context");
-    jmethodID getSys = (*env)->GetMethodID(env, ctx, "getSystemService", "(Ljava/lang/String;)Ljava/lang/Object;");
-    jobject cb = (*env)->CallObjectMethod(env, app->activity->clazz, getSys, (*env)->NewStringUTF(env, "clipboard"));
-    jclass cd = (*env)->FindClass(env, "android/content/ClipData");
-    jmethodID newT = (*env)->GetStaticMethodID(env, cd, "newPlainText", "(Ljava/lang/CharSequence;Ljava/lang/CharSequence;)Landroid/content/ClipData;");
-    jobject clip = (*env)->CallStaticObjectMethod(env, cd, newT, (*env)->NewStringUTF(env, "Err"), (*env)->NewStringUTF(env, text));
-    jclass cm = (*env)->FindClass(env, "android/content/ClipboardManager");
-    (*env)->CallVoidMethod(env, cb, (*env)->GetMethodID(env, cm, "setPrimaryClip", "(Landroid/content/ClipData;)V"), clip);
-    (*app->activity->vm)->DetachCurrentThread(app->activity->vm);
-}
-
 unsigned int game_seed = 0;
 bool fatal_error = false;
 
@@ -56,7 +42,7 @@ static void engine_handle_cmd(struct android_app* app, int32_t cmd) {
         eglChooseConfig(eng->display, att, &config, 1, &n);
         eng->surface = eglCreateWindowSurface(eng->display, config, app->window, NULL);
         eng->context = eglCreateContext(eng->display, config, NULL, (EGLint[]){EGL_CONTEXT_CLIENT_VERSION, 2, EGL_NONE});
-        if (!eglMakeCurrent(eng->display, eng->surface, eng->surface, eng->context)) { fatal_error=true; copy_to_clipboard(app, "EGL Fail"); return; }
+        eglMakeCurrent(eng->display, eng->surface, eng->surface, eng->context);
 
         const char* vS = "attribute vec3 pos; attribute vec2 uv; attribute vec3 norm; attribute float type; varying vec2 vUV; varying float vL; varying float vT; varying float vNY; uniform mat4 m; void main(){ gl_Position=m*vec4(pos,1.0); vUV=uv; vT=type; vNY=norm.y; vL=clamp(dot(norm, vec3(0.4,1.0,0.3)), 0.4, 1.0); }";
         const char* fS = "precision mediump float; varying vec2 vUV; varying float vL; varying float vT; varying float vNY; uniform sampler2D uT1, uT2, uT3, uT4, uT5, uT6; void main(){ vec4 c; if(vT > 2.5){ c=texture2D(uT4, vUV); if(c.a < 0.1) discard; } else if(vT > 1.5){ if(vNY>0.5 || vNY<-0.5) c=texture2D(uT6, vUV); else c=texture2D(uT5, vUV); } else { if(vNY>0.5) c=texture2D(uT1, vUV); else if(vNY<-0.5) c=texture2D(uT3, vUV); else c=texture2D(uT2, vUV); } gl_FragColor=vec4(c.rgb*vL, c.a); }";
@@ -76,7 +62,7 @@ void android_main(struct android_app* state) {
     memset(eng, 0, sizeof(struct engine));
     eng->app = state; eng->gameState = STATE_MENU;
     eng->invSlots[0]=BLOCK_GRASS; eng->invSlots[1]=BLOCK_WOOD; eng->invSlots[2]=BLOCK_LEAVES;
-    eng->camPos[1]=20.0f; state->userData = eng; state->onAppCmd = engine_handle_cmd; state->onInputEvent = engine_handle_input;
+    eng->camPos[1]=15.0f; state->userData = eng; state->onAppCmd = engine_handle_cmd; state->onInputEvent = engine_handle_input;
     while (1) {
         int ev; struct android_poll_source* s;
         while (ALooper_pollOnce(0, NULL, &ev, (void**)&s) >= 0) { if (s) s->process(state, s); if (state->destroyRequested) { free(eng); return; } }
