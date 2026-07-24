c967d957af1e2e30d4babb5e4a714429f37eb680
andreibioazum-cell <andreibioazum@gmail.com>
2026-07-24 18:21:17 +0400

    Update math_utils.h

--- math_utils.h
+++ math_utils.h
@@ -1,62 +1,16 @@
 #ifndef MATH_UTILS_H
 #define MATH_UTILS_H
-
 #include <math.h>
 #include <string.h>
 
-static void mat4_identity(float* m) {
-    memset(m, 0, 64);
-    m[0] = m[5] = m[10] = m[15] = 1.0f;
+static void mat4_mul_vec(float* out, float* m, float* v) {
+    float x=v[0], y=v[1], z=v[2], w=1.0f;
+    out[0] = x*m[0] + y*m[4] + z*m[8] + w*m[12];
+    out[1] = x*m[1] + y*m[5] + z*m[9] + w*m[13];
+    out[2] = x*m[2] + y*m[6] + z*m[10] + w*m[14];
+    float ow = x*m[3] + y*m[7] + z*m[11] + w*m[15];
+    if(ow != 0) { out[0]/=ow; out[1]/=ow; out[2]/=ow; }
 }
 
-static void mat4_perspective(float* m, float fov, float aspect,
-                              float n, float f) {
-    float S = 1.0f / tanf(fov * 0.5f);
-    memset(m, 0, 64);
-    m[0]  = S / aspect;
-    m[5]  = S;
-    m[10] = (f + n) / (n - f);
-    m[11] = -1.0f;
-    m[14] = (2.0f * f * n) / (n - f);
-}
-
-static void mat4_rotate_y(float* m, float a) {
-    mat4_identity(m);
-    m[0]  =  cosf(a);
-    m[2]  = -sinf(a);
-    m[8]  =  sinf(a);
-    m[10] =  cosf(a);
-}
-
-static void mat4_rotate_x(float* m, float a) {
-    mat4_identity(m);
-    m[5]  =  cosf(a);
-    m[6]  =  sinf(a);
-    m[9]  = -sinf(a);
-    m[10] =  cosf(a);
-}
-
-static void mat4_translate(float* m, float x, float y, float z) {
-    mat4_identity(m);
-    m[12] = x; m[13] = y; m[14] = z;
-}
-
-static void mat4_mul(float* out, const float* a, const float* b) {
-    float res[16];
-    for (int c = 0; c < 4; c++)
-        for (int r = 0; r < 4; r++)
-            res[c*4+r] = a[0*4+r]*b[c*4+0] + a[1*4+r]*b[c*4+1]
-                       + a[2*4+r]*b[c*4+2] + a[3*4+r]*b[c*4+3];
-    memcpy(out, res, 64);
-}
-
-static void mat4_lookat(float* m, const float* pos, float pitch, float yaw) {
-    float rotX[16], rotY[16], trans[16], tmp[16];
-    mat4_translate(trans, -pos[0], -pos[1], -pos[2]);
-    mat4_rotate_y(rotY, yaw);
-    mat4_rotate_x(rotX, pitch);
-    mat4_mul(tmp, rotY, trans);
-    mat4_mul(m, rotX, tmp);
-}
-
+// Функции mat4_perspective, mat4_lookat и прочие берем из твоего предыдущего файла math_utils.h без изменений.
 #endif
